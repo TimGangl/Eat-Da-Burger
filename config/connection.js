924f838975e58e9c118ddbfd7493a789df31ed82
Tim Gangl <tim.gangl30@gmail.com>
2020-01-13 16:23:51 -0800

    fixing heroku app i hope

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+// Connect Node to MySQL.
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  port: 3306,
+  password: "root",
+  database: "burgers_db"
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("Connected as ID: " + connection.threadId);
+});
+
+// Export the connection.
+module.exports = connection;

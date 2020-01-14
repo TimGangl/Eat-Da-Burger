abeb7ae68e16eef464e8a4319899526434a757b3
Tim Gangl <tim.gangl30@gmail.com>
2020-01-14 15:38:19 -0800

    hope I fixed heroku issue

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Connect Node to MySQL.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  port: 3306,
-  password: "root",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    port: 3306,
+    password: "root",
+    database: "burgers_db"
+  });
+}
 
 connection.connect(function (err) {
   if (err) {
